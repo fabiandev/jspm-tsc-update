117ba6d435f5453aeb205926ad6108b1f1caef41
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-07-02 01:18:16 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+# jspm-tsc-update
+
+This package was created out of the need to also have installed jspm packages 
+mapped in tsconfig.json `compilerOptions.paths`.
+
+# Installation
+
+```sh
+$ yarn global add jspm-tsc-update
+```
+
+> Or you can use: `npm install -g jspm-tsc-update`
+
+# Global Usage
+
+Make sure to be in the root of your project, where your `package.json` exists.
+Also the files `system.config.js` and `tsconfig.json` must exist at this location.
+
+```
+$ jspm-tsc-update
+```
+
+After executing the command from above, the `compilerOptions.paths` in your `tsconfig.json` have been updated.
+A a file named `pathmap.json` was created to keep track of deleted jspm packages and to safely remove them from
+your TypeScript path mappings.
+
+# Local Usage
+
+You may also install this package locally via `yarn add jspm-tsc-update` or `npm install --save-dev jspm-tsc-update`.
+To create an alias for running the executable, add something like this to your `package.json` `scripts`:
+
+```json
+{
+  "scripts": {
+    "update-paths": "node_modules/.bin/jspm-tsc-update"
+  }
+}
+```
+
+Now you just have to make sure to run this command alongside installing or uninstalling with jspm:
+
+```
+$ jspm install css-animator && yarn update-paths
+```
+
+> Or when using npm instead of yarn: `npm run update-paths`
---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+console.log('Creating tsconfig path mappings from systemjs..');
+
+const fs = require('fs');
+const path = require('path');
+const Builder = require('systemjs-builder');
+
+const tsConfigPath = path.join(process.cwd(), 'tsconfig.json');
+const pathMapPath = path.join(process.cwd(), 'pathmap.json');
+const tsConfig = require(tsConfigPath);
+
+if (!fs.existsSync(pathMapPath)) {
+  fs.writeFileSync(pathMapPath, JSON.stringify({
+    packages: []
+  }, null, '\t'));
+}
+
+const pathMap = require(pathMapPath);
+
+const builder = new Builder();
+
+const paths = {};
+const merged = {};
+
+builder.loadConfig('./system.config.js')
+  .then(mapPaths)
+  .then(mergePaths)
+  .then(writePaths)
+  .then(() => {
+    console.log('tsconfig.json has been updated.');
+    process.exit(0);
+  }).catch(reason => {
+    console.error(reason.message
+      ? reason.message
+      : reason);
+    process.exit(1);
+  });
+
+function mapPaths() {
+  for (let map in builder.loader.map) {
+    const normalized = builder.loader.normalizeSync(map);
+    const relative = normalized.replace(builder.loader.baseURL, '');
+    paths[map] = [relative]
+    paths[`${map}/*`] = [`${relative}/*`, `${relative}/*/index`];
+  }
+}
+
+function mergePaths() {
+  const deleted = [];
+
+  for (let path of pathMap.packages) {
+    if (!builder.loader.map.hasOwnProperty(path)) {
+      deleted.push(path);
+      deleted.push(`${path}/*`);
+    }
+  }
+
+  if (!tsConfig.compilerOptions) {
+    tsConfig.compilerOptions = {};
+  }
+
+  if (!tsConfig.compilerOptions.paths) {
+    tsConfig.compilerOptions.paths = {};
+  }
+  if (!tsConfig.compilerOptions.baseUrl) {
+    tsConfig.compilerOptions.baseUrl = '.';
+  }
+
+  for (let path in tsConfig.compilerOptions.paths) {
+    if (deleted.indexOf(path) === -1) {
+      merged[path] = tsConfig.compilerOptions.paths[path];
+    }
+  }
+
+  for (let path in paths) {
+    merged[path] = paths[path];
+  }
+}
+
+function writePaths() {
+  tsConfig.compilerOptions.paths = merged;
+  pathMap.packages = Object.keys(builder.loader.map).map(key => key);
+  fs.writeFileSync(tsConfigPath, JSON.stringify(tsConfig, null, '\t'));
+  fs.writeFileSync(pathMapPath, JSON.stringify(pathMap, null, '\t'))
+}
