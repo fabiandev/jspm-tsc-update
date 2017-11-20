37f813f1b7c84a79d90461aaec1072bc34f8d9d4
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-11-20 17:15:54 +0100

    Introduced API options and general improvements.

---
+++ defaults.js
@@ -0,0 +1,8 @@
+module.exports = {
+  silent: false,
+  tsConfigName: 'tsconfig',
+  jspmConfigName: 'tsconfig.jspm',
+  packagePath: process.cwd(),
+  tsConfigPath: './',
+  jspmConfigPath: './',
+};
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function () {
-  console.log('Creating tsconfig path mappings from jspm..');
+module.exports = function (options) {
+  const jspm = require('jspm');
+  jspm.setPackagePath(opts.packagePath);
+  
+  const log = function log(text, method) {
+    if (opts.silent) return;
+    method = !method ? 'log': method;
+    console[method](`jspm-tsc-update: ${text}`);
+  };
 
-  const tsConfigName = 'tsconfig';
-  const tsConfigJspmName = 'tsconfig.jspm';
-  const tsConfigJspmPath = './';
+  const defaults = require('./defaults');
+  const opts = Object.assign({}, defaults, options);
 
-  const tsConfigPath = path.join(process.cwd(), `${tsConfigName}.json`);
-  const pathMapPath = path.join(process.cwd(), `${tsConfigJspmName}.json`);
-  const tsConfig = require(tsConfigPath);
+  const tsConfigPath = path.resolve(opts.packagePath, opts.tsConfigPath);
+  const jspmConfigPath = path.resolve(opts.packagePath, opts.jspmConfigPath);
+
+  const tsConfigFile = path.join(tsConfigPath, `${opts.tsConfigName}.json`);
+  const jspmConfigFile = path.join(jspmConfigPath, `${opts.jspmConfigName}.json`);
+
+  const tsConfig = require(tsConfigFile);
   const builder = new (require('jspm').Builder)();
 
   try {
-    var paths = {};
+    let paths = {};
     for (let map in builder.loader.map) {
       const normalized = builder.loader.normalizeSync(map);
       const relative = normalized.replace(builder.loader.baseURL, '');
@@ -22,25 +32,28 @@ module.exports = function () {
       paths[`${map}/*`] = [`${relative}/*`, `${relative}/*/index`];
     }
 
-    var pathMap = { compilerOptions: { baseUrl: '.', paths: paths } };
-    fs.writeFileSync(pathMapPath, JSON.stringify(pathMap, null, '\t'));
-    console.log(`${tsConfigJspmName}.json has been updated.`);
+    const pathMap = { compilerOptions: { baseUrl: '.', paths: paths } };
+    fs.writeFileSync(jspmConfigFile, JSON.stringify(pathMap, null, '\t'));
+    log(`${opts.jspmConfigName}.json has been created.`);
+
+    const extendsPath = path.relative(tsConfigPath, jspmConfigPath);
+    const extendsFile = (!extendsPath ? './' : extendsPath) + opts.jspmConfigName;
 
     if (!tsConfig.extends) {
-      tsConfig.extends = `${tsConfigJspmPath}${tsConfigJspmName}`;
-      fs.writeFileSync(tsConfigPath, JSON.stringify(tsConfig, null, '\t'));
-      console.log(`${tsConfigName}.json has been updated.`);
+      tsConfig.extends = extendsFile;
+      fs.writeFileSync(tsConfigFile, JSON.stringify(tsConfig, null, '\t'));
+      log(`${opts.tsConfigName}.json has been updated.`);
     }
-    
-    if (tsConfig.extends !== `${tsConfigJspmPath}${tsConfigJspmName}`) {
-      console.warn(`${tsConfigName}.json does not extend ${tsConfigJspmName}.json`);
+
+    if (tsConfig.extends && tsConfig.extends !== extendsFile) {
+      log(`${opts.tsConfigName}.json does not extend ${opts.jspmConfigName}.json`, 'warn');
     }
 
     return true;
   }
   catch (reason) {
-    console.error(reason.message || reason);
+    log(reason.message || reason, 'error');
   }
-  
+
   return false;
 }
