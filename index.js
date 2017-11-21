297b9c96b878d48ddadca30f2f65b44bad4efaf7
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-11-21 15:02:43 +0100

    Get around tsconfig extends options overwriting paths entirely.

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function (options) {
-  const jspm = require('jspm');
-  jspm.setPackagePath(opts.packagePath);
-  
   const log = function log(text, method) {
     if (opts.silent) return;
     method = !method ? 'log': method;
@@ -13,12 +10,20 @@ module.exports = function (options) {
 
   const defaults = require('./defaults');
   const opts = Object.assign({}, defaults, options);
+  
+  const jspm = require('jspm');
+  jspm.setPackagePath(opts.packagePath);
 
   const tsConfigPath = path.resolve(opts.packagePath, opts.tsConfigPath);
   const jspmConfigPath = path.resolve(opts.packagePath, opts.jspmConfigPath);
 
   const tsConfigFile = path.join(tsConfigPath, `${opts.tsConfigName}.json`);
   const jspmConfigFile = path.join(jspmConfigPath, `${opts.jspmConfigName}.json`);
+  
+  if (!fs.existsSync(tsConfigFile)) {
+    log(`${opts.tsConfigName}.json does not exist.`, 'error');
+    return false;
+  }
 
   const tsConfig = require(tsConfigFile);
   const builder = new (require('jspm').Builder)();
@@ -32,22 +37,26 @@ module.exports = function (options) {
       paths[`${map}/*`] = [`${relative}/*`, `${relative}/*/index`];
     }
 
-    const pathMap = { compilerOptions: { baseUrl: '.', paths: paths } };
-    fs.writeFileSync(jspmConfigFile, JSON.stringify(pathMap, null, '\t'));
+    const pathsMap = Object.assign(tsConfig.compilerOptions.paths, paths);
+    const extendsPath = path.relative(jspmConfigPath, tsConfigPath);
+    const extendsFile = (!extendsPath ? './' : extendsPath) + opts.tsConfigName;
+    const config = { extends: '', compilerOptions: {} };
+    
+    if (!config.compilerOptions.baseUrl) {
+      config.compilerOptions.baseUrl = opts.baseUrl;
+    }
+    
+    config.extends = extendsFile;
+    config.compilerOptions.paths = pathsMap;
+    
+    if (opts.backupTsConfig && fs.existsSync(jspmConfigFile)) {
+      let name = `${opts.backupPrefix}${opts.jspmConfigName}.json${opts.backupPostfix}`;
+      fs.writeFileSync(name, JSON.stringify(require(jspmConfigFile), null, '\t'));
+      log(`${name} has been created.`);
+    }
+    
+    fs.writeFileSync(jspmConfigFile, JSON.stringify(config, null, '\t'));
     log(`${opts.jspmConfigName}.json has been created.`);
-
-    const extendsPath = path.relative(tsConfigPath, jspmConfigPath);
-    const extendsFile = (!extendsPath ? './' : extendsPath) + opts.jspmConfigName;
-
-    if (!tsConfig.extends) {
-      tsConfig.extends = extendsFile;
-      fs.writeFileSync(tsConfigFile, JSON.stringify(tsConfig, null, '\t'));
-      log(`${opts.tsConfigName}.json has been updated.`);
-    }
-
-    if (tsConfig.extends && tsConfig.extends !== extendsFile) {
-      log(`${opts.tsConfigName}.json does not extend ${opts.jspmConfigName}.json`, 'warn');
-    }
 
     return true;
   }
