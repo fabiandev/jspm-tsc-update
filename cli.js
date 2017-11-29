8c259df6a7a2ddc05437e6802217997535d1b02e
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-11-29 10:41:24 +0100

    Removed minimist in favor for yargs-parser. Resolves #4.

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require('yargs-parser')(process.argv.slice(2));
 const updateTsConfig = require('./index');
 const e = updateTsConfig(argv);
 process.exit(e ? 0 : 1);
